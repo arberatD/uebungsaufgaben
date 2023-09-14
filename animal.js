20008948a1c0c2e73c5b3f8675ec1ae86a7ef2b7
arberatD <arberatdushi@gmail.com>
2023-09-14 15:21:39 +0200

    hide div´s in html

--- animal.js
+++ animal.js
@@ -6,5 +6,27 @@ class animal {
     this.weight = weight;
     this.alive = true;
    } 
+
+   feed (amount){
+        this.weight += amount;
+   }
+}
+class Predator extends animal {
+    constructor(name, weight, dangerfactor = 1){
+        super(name, weigt)
+        this.dangerfactor = dangerfactor
+    }
 }
 
+
+class Prey extends animal {
+
+}
+
+let sheep = new Prey("Shaun", 35)
+sheep.feed(3)
+console.log(sheep.weight)
+console.log(sheep instanceof Prey)
+console.log(sheep instanceof animal)
+let lion = new Predator ("Leon", 250)
+console.log(lion.weight)
